bccdfb6c83fcb3cb56305c25a10776e3b1d2cf2e
p sc <patty.stacruz@gmail.com>
2016-10-21 02:52:11 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
+	loop do
+		puts "What is the spell that enacts levitation?"
+		spell = gets.chomp
+
+		break if spell == "Wingardium Leviosa"
+	end
+
+	puts "You passed the quiz!"
+
 	#your code here
 end
-
-
